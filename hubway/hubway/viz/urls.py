fcaaa7e57097b8f85f93314d33eeb3b809080e14
David Zwarg <david.zwarg@gmail.com>
2012-10-13 21:07:21 -0400

    Updated pipe to be a 20 line overview

--- hubway/hubway/viz/urls.py
+++ hubway/hubway/viz/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', views.home, name='home'),
     url(r'counts/$', views.counts, name='counts'),
+    url(r'trips/(?P<number>.+)$', views.trips, name='trips'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- hubway/hubway/viz/views.py
+++ hubway/hubway/viz/views.py
@@ -8,11 +8,37 @@ import logging
 logger = logging.getLogger(__name__)
 
 def home(request):
+    """
+    The initial page for the web application.
+    """
     return render_to_response('index.html', mimetype='text/html')
 
 
 # Create your views here.
 def counts(request):
+    """
+    A web 'handler' that returns the number of rides per bike.
+    """
     counts = { 'counts': Trip.objects.values('bike_nr').annotate(bike_cnt=Count('bike_nr')).order_by('-bike_cnt') }
 
     return render_to_response('counts.json', counts, mimetype='application/json')
+
+
+def trips(request, number):
+    """
+    A web 'handler' that returns the trips of a single bicycle.
+    """
+    trips = Trip.objects.filter(bike_nr=number).order_by('start_date')
+    
+    def to_millis(x):
+        return { 
+            'start_date': x.start_date.isoformat(),
+            'end_date': x.end_date.isoformat(),
+            'gender': x.gender
+        }
+        
+    trips = map(to_millis, trips)
+    
+    trips = { 'trips': trips }
+    
+    return render_to_response('trips.json', trips, mimetype='application/json')
